035e7d70045cbb8247c84b8485a45b26000c8b5a
elhassanianas <elhassani.anas06@gmail.com>
2023-04-17 13:46:35 +0000

    homeAnas space

--- src/pages/Homeanas.js
+++ src/pages/Homeanas.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
   const Home = () => {
  
     return (
+      
       <div className="p-6 sm:ml-64">
           <div className=" flex items-center justify-center p-4 border-2 border-gray-200 border rounded-lg dark:border-gray-700 mt-20">
             {/* <div className="max-w-sm">
